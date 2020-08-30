bd1cc48a65e61438f8837625075c0b7891a9a543
Dean Simon Damasig <ddamasig@gmail.com>
2020-08-30 16:52:51 +0800

    Added item attribute for AppBarBrowse items

--- components/AppBarBrowse.vue
+++ components/AppBarBrowse.vue
@@ -33,7 +33,19 @@
           outlined
           hide-details
           label="Type keywords here"
-        />
+          :items="books"
+          item-text="title"
+          item-value="title"
+        >
+          <template v-slot:item="data">
+            <v-list-item-content>
+              <v-list-item-title light v-text="data.item.title" />
+              <v-list-item-subtitle>
+                by {{ data.item.author.fullname }} on {{ data.item.date_published }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-autocomplete>
       </v-col>
     </v-row>
   </v-app-bar>
@@ -50,6 +62,51 @@ export default {
         text: 'Mathematics'
       }, {
         text: 'Algebra'
+      }],
+      books: [{
+        id: 1,
+        title: 'Learner-Centered Leadership A Blueprint for Transformational Change in Learning Communities',
+        description: 'The pace of change in our society is currently accelerating. Old concepts of authority and hierarchy have been challenged by the reality of a networked world, and bedrock ideas about what citizens need to know have been overturned by a constant drive to learn anew. For Devin Vodicka, the response to these challenges is both simple andall-encompassing: it is the learner who is constant in the new world, and therefore educators must empower the learner to drive their journey',
+        author: {
+          fullname: 'Devin Vodicka'
+        },
+        date_published: 'April 18, 2020',
+        rating: 4.7,
+        favorite_at: 'April 18, 2020',
+        thumbnail: 'https://picsum.photos/300/200'
+      }, {
+        id: 2,
+        title: 'Professional Development of Teacher Educators in Further Education',
+        description: 'Professional Development of Teacher Educators in Further Education critically analyses the specific challenges relating to teacher educators in the English further education (FE), such as the diverse nature of learners and the variety of educational contexts. It focuses on the journeys to becoming teacher educators, their relevant teaching know-how and professional needs. This book combines theoretical frameworks with both qualitative and quantitative data to outline the pathways, professional identities, knowledge, and continuous professional development of teacher educators. This data is used to discuss the four main themes',
+        author: {
+          fullname: 'Sia Loo'
+        },
+        date_published: 'April 18, 2020',
+        rating: 4.7,
+        favorite_at: 'April 18, 2020',
+        thumbnail: 'https://picsum.photos/300/201'
+      }, {
+        id: 1,
+        title: 'Learner-Centered Leadership A Blueprint for Transformational Change in Learning Communities',
+        description: 'The pace of change in our society is currently accelerating. Old concepts of authority and hierarchy have been challenged by the reality of a networked world, and bedrock ideas about what citizens need to know have been overturned by a constant drive to learn anew. For Devin Vodicka, the response to these challenges is both simple andall-encompassing: it is the learner who is constant in the new world, and therefore educators must empower the learner to drive their journey',
+        author: {
+          fullname: 'Devin Vodicka'
+        },
+        date_published: 'April 18, 2020',
+        rating: 4.7,
+        favorite_at: 'April 18, 2020',
+        thumbnail: 'https://picsum.photos/300/202'
+      }, {
+        id: 1,
+        title: 'Learner-Centered Leadership A Blueprint for Transformational Change in Learning Communities',
+        description: 'The pace of change in our society is currently accelerating. Old concepts of authority and hierarchy have been challenged by the reality of a networked world, and bedrock ideas about what citizens need to know have been overturned by a constant drive to learn anew. For Devin Vodicka, the response to these challenges is both simple andall-encompassing: it is the learner who is constant in the new world, and therefore educators must empower the learner to drive their journey',
+        author: {
+          fullname: 'Devin Vodicka'
+        },
+        date_published: 'April 18, 2020',
+        rating: 4.7,
+        favorite_at: 'April 18, 2020',
+        thumbnail: 'https://picsum.photos/300/203'
       }]
     }
   }
