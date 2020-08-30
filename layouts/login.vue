018e767ef27573daa09ac30f1c20a645e47ecbfc
Dean Simon Damasig <ddamasig@gmail.com>
2020-08-30 18:10:58 +0800

    Added login page

---
+++ layouts/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  .line-clamp-1 {
+    line-clamp: 1;
+  }
+
+  .line-clamp-2 {
+    line-clamp: 2;
+  }
+
+  .line-clamp-3 {
+    line-clamp: 3 !important;
+  }
+  ::-webkit-scrollbar {
+    width: 0px;
+    background: transparent; /* make scrollbar transparent */
+  }
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container class="fill-height">
+    <v-row class="align-center">
+      <v-col class="text-center" cols="12">
+        <h1 class="accent--text">
+          <v-icon size="50" class="mb-1" color="primary">
+            mdi-book-open-blank-variant
+          </v-icon>
+          Books.io
+        </h1>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          filled
+          rounded
+          label="Email"
+          type="email"
+          single-line
+          hide-details
+          prepend-inner-icon="mdi-email"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          filled
+          rounded
+          label="Password"
+          type="password"
+          single-line
+          hide-details
+          prepend-inner-icon="mdi-lock"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          color="primary"
+          large
+          block
+          rounded
+        >
+          <v-icon class="mr-1">
+            mdi-login
+          </v-icon>
+          Login
+        </v-btn>
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          large
+          class="accent--text"
+          block
+          rounded
+          text
+        >
+          <v-icon class="mr-1">
+            mdi-account-plus
+          </v-icon>
+          Create an Account
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  layout: 'login',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
